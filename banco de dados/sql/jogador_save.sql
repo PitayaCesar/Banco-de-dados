8840161917ff047fd3b30e7ffbb8a3f78bd52397
João Carlos <52039289+Jhow-X@users.noreply.github.com>
2021-06-30 18:09:29 -0300

    Update jogador_save.sql

--- banco de dados/sql/jogador_save.sql
+++ banco de dados/sql/jogador_save.sql
@@ -1,9 +1,6 @@
 CREATE TABLE Jogador_Save(
-	Save_Cod Integer NOT NULL UNIQUE,
 	Jog_Id VARCHAR(3) NOT NULL UNIQUE,
 	Nick VARCHAR(8) NOT NULL,
-	Nivel Integer NOT NULL,
 	Score Integer NOT NULL,
-	PRIMARY KEY(Save_Cod,Jog_id)
 )
 
